919424fad8a70fe2f7c54dcf35a4e8cb276d0b3b
vic1er03 <azariaazaria473@gmail.com>
2025-03-24 20:06:57 +0100

    Update Das.py

--- Das.py
+++ Das.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
-url = Path(__file__).parent / "Challenge_dataset_traité.csv"
-data = pd.read_csv(url,sep=';')
+url = "data_2019_preprocessed.csv"
+data = pd.read_csv(url)
 st.dataframe(data)
 st.DataFrame(data)
