dee3f984bad7f1e01fee2e62bd41656443e66260
vic1er03 <azariaazaria473@gmail.com>
2025-03-24 19:11:30 +0100

    Update Das.py

--- Das.py
+++ Das.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import os
+print("Current working directory:", os.getcwd())
 st.title("vic")
 url = r".devcontainer/Challenge_dataset_traité.csv"
 data = pd.read_csv(url,sep=';')
