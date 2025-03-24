6f522bdd82e617d3baea844184b0ada78551334b
vic1er03 <azariaazaria473@gmail.com>
2025-03-24 17:50:45 +0100

    Update Das.py

--- Das.py
+++ Das.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 st.title("vic")
-paths="Challenge_dataset_traité.csv"
+paths=r".\Challenge_dataset_traité.csv"
 data=pd.read_csv(paths)
 st.DataFrame(data)
