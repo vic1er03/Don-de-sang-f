475be1de9d2fa16dff67e0ae161926540751250f
vic1er03 <azariaazaria473@gmail.com>
2025-03-24 20:09:37 +0100

    Update Das.py

--- Das.py
+++ Das.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 url = "data_2019_preprocessed.csv"
-data = pd.read_csv(url)
+data = pd.read_csv(url,sep=';')
 st.dataframe(data)
 st.DataFrame(data)
